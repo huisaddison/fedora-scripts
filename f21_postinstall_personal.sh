2110b733e15bb462a07ac1875c67d70bbec5d581
David Goerger <dgoerger@users.noreply.github.com>
2015-01-18 23:22:09 -0500

    cleanup

--- f21_postinstall_personal.sh
+++ f21_postinstall_personal.sh
@@ -2,23 +2,25 @@
 # for a fresh Fedora 21 Workstation install; do not use on already-in-use systems
 
 ### remove stuff I don't need
-sudo dnf erase -y abrt* bijiben cheese devassistant evolution gnome-documents java* libreoffice* orca rhythmbox transmission-gtk
+sudo dnf erase -y abrt* bijiben cheese devassistant evolution gnome-boxes gnome-documents java* libreoffice* libvirt* orca qemu* rhythmbox transmission-gtk
 
 ### enable moar repos
 sudo dnf copr enable -y dgoerger/firefox-gtk3
 sudo dnf copr enable -y petersen/pandoc
 sudo dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-21.noarch.rpm
+# sadly necessary for broadcom-wl...
+sudo dnf install -y http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-21.noarch.rpm
 
 ### make sure everything's up-to-date
 sudo dnf upgrade -y
 
 ### install drivers things
-sudo dnf install -y mesa-vdpau-drivers libva-vdpau-driver libvdpau-va-gl powertop redshift
+sudo dnf install -y broadcom-wl kmod-wl mesa-vdpau-drivers libtxc_dxtn libva-vdpau-driver libvdpau-va-gl powertop redshift
 # set redshift DISPLAY variable to built-in monitor per https://bbs.archlinux.org/viewtopic.php?pid=1447538#p1447538
 mkdir -p $HOME/.config/systemd/user/redshift.service.d
 # use DISPLAY=:1 for second display, :2 for third, etc
 echo '[Service]
-Environment=DISPLAY=:0' > $HOME/.config/systemd/user/redshift.service.d/display.conf
+Environment=DISPLAY=:0' | tee $HOME/.config/systemd/user/redshift.service.d/display.conf
 # set redshift location since geoclue lookup relies on Internet which is not initialized in time usually
 echo '; Global settings for redshift
 [redshift]
@@ -26,7 +28,7 @@ location-provider=manual
 
 [manual]
 lat=41.31
-lon=-72.923611' > $HOME/.config/redshift.conf
+lon=-72.923611' | tee $HOME/.config/redshift.conf
 # enable redshift service
 systemctl --user enable redshift.service
 # enable powertop service
@@ -36,13 +38,13 @@ sudo systemctl enable powertop.service
 sudo dnf install -y git lynx pandoc screen transmission-cli vim-enhanced
 # sane vimrc
 echo 'set nocompatible
-syntax on' > $HOME/.vimrc
+syntax on' | tee $HOME/.vimrc
 # recognize epubs as zips for editing in vim
-echo 'au BufReadCmd   *.epub      call zip#Browse(expand("<amatch>"))' >> $HOME/.vimrc
+echo 'au BufReadCmd   *.epub      call zip#Browse(expand("<amatch>"))' | tee --append $HOME/.vimrc
 # sane screenrc
 echo 'caption always
 termcapinfo xterm*|rxvt*|kterm*|Eterm*|putty*|dtterm* ti@:te@
-defscrollback 20736' > $HOME/.screenrc
+defscrollback 20736' | tee $HOME/.screenrc
 # sanitize bash_profile $PATH to disallow ~/bin and ~/.local/bin
 echo '# .bash_profile
 
@@ -51,23 +53,17 @@ if [ -f ~/.bashrc ]; then
 	. ~/.bashrc
 fi
 
-# User specific environment and startup programs' > $HOME/.bash_profile
+# User specific environment and startup programs' | tee $HOME/.bash_profile
 
 ### set up rpmbuild environment
 # per https://fedoraproject.org/wiki/How_to_create_an_RPM_package
-sudo dnf install @Development\ Tools
-sudo dnf install fedora-packager
+sudo dnf install -y @Development\ Tools
+sudo dnf install -y fedora-packager
 rpmdev-setuptree
 
 ### media libs
 sudo dnf install -y gstreamer1-libav gstreamer1-plugins-ugly gstreamer1-plugins-bad-freeworld \
                     gstreamer1-plugins-bad-free
-
-### HandBrake
-## libdvdnav doesn't seem to work with totem? workaround:
-## HandBrakeCLI -Z "Normal" -i /dev/sr0 -o $NAME.mp4 -t $NUM
-# sudo yum-config-manager --add-repo=http://negativo17.org/repos/fedora-handbrake.repo
-# sudo dnf install HandBrake-cli
 
 ### spellcheck dictionaries
 sudo dnf install -y hunspell-en hunspell-es hunspell-de hunspell-fr hunspell-nn
@@ -81,7 +77,7 @@ sudo dnf install -y vim-latex vim-latex-doc texlive-collection-basic texlive-col
 
 ### general apps
 # TODO: duplicity instead of deja-dup ?
-sudo dnf install -y calibre deja-dup empathy epiphany firewalld gnome-boxes gnome-contacts \
+sudo dnf install -y calibre deja-dup empathy epiphany firewalld gnome-contacts \
                     gnome-music gnome-weather gnumeric keepassx shotwell 
 
 ### GNOME tweaks
@@ -89,17 +85,23 @@ sudo dnf install -y gnome-shell-extension-alternate-tab gnome-tweak-tool
 # set global gtk3 dark theme
 mkdir -p $HOME/.config/gtk-3.0
 echo '[Settings]
-gtk-application-prefer-dark-theme=1' > $HOME/.config/gtk-3.0/settings.ini
+gtk-application-prefer-dark-theme=1' | tee $HOME/.config/gtk-3.0/settings.ini
 # enable the alternate-tab extension we just installed
 # sets alt-tab on windows, not applications
 dconf write /org/gnome/shell/enabled-extensions "['alternate-tab@gnome-shell-extensions.gcampax.github.com']"
+# display date in top bar of Shell
+dconf write /org/gnome/desktop/interface/clock-show-date true
 # set Terminal dark theme. is this redundant? dark seems the default in 3.14?
-dconf write /org/gnome/terminal/legacy/dark-theme true
+#dconf write /org/gnome/terminal/legacy/dark-theme true
 # hide Terminal menu bar
 dconf write /org/gnome/terminal/legacy/default-show-menubar false
 # sane trackpad settings
 dconf write /org/gnome/settings-daemon/peripherals/touchpad/natural-scroll true  # Mac-style scrolling
 dconf write /org/gnome/settings-daemon/peripherals/touchpad/tap-to-click true
+# set default indexing for tracker
+dconf write /org/freedesktop/tracker/miner/files/index-recursive-directories "['&DESKTOP', '&DOCUMENTS', '&MUSIC', '&PICTURES', '&VIDEOS']"
+# disable autorun on media insertion
+dconf write /org/gnome/desktop/media-handling/autorun-never true
 
 ### moar dconf tweaks
 # shotwell
@@ -112,26 +114,25 @@ dconf write /org/yorba/shotwell/preferences/ui/hide-photos-already-imported true
 ## global extensions
 sudo dnf install -y mozilla-https-everywhere mozilla-noscript
 ## local settings - set here to survive upgrades and not disturb other user accounts
-## TODO: is there a /usr/lib64/firefox/.../defaults/user.js file we can set these 
-##       in for global default prefs which survive upgrades?
 # create local profile
 firefox -CreateProfile default >> /dev/null
 # grab firefox profile name
 FF=`grep Path $HOME/.mozilla/firefox/profiles.ini | sed 's/Path\=//'`
+FFPATH=$HOME/.mozilla/firefox/${FF}
 # neuter the hazard of 'ctrl+q'
-echo 'user_pref("browser.showQuitWarning", true);' | tee --append $HOME/.mozilla/firefox/${FF}/prefs.js >> /dev/null
+echo 'user_pref("browser.showQuitWarning", true);' | tee --append ${FFPATH}/prefs.js
 # disable 'sponsored tiles'
-echo 'user_pref("browser.newtabpage.directory.ping", "");' | tee --append $HOME/.mozilla/firefox/${FF}/prefs.js >> /dev/null
-echo 'user_pref("browser.newtabpage.directory.source", "");' | tee --append $HOME/.mozilla/firefox/${FF}/prefs.js >> /dev/null
+echo 'user_pref("browser.newtabpage.directory.ping", "");' | tee --append ${FFPATH}/prefs.js
+echo 'user_pref("browser.newtabpage.directory.source", "");' | tee --append ${FFPATH}/prefs.js
 # set DONOTTRACK header
-echo 'user_pref("privacy.donottrackheader.enabled", true);' | tee --append $HOME/.mozilla/firefox/${FF}/prefs.js >> /dev/null
+echo 'user_pref("privacy.donottrackheader.enabled", true);' | tee --append ${FFPATH}/prefs.js
 # set spellcheck language as Canadian English moz#836230
-echo 'user_pref("spellchecker.dictionary", "en_CA");' | tee --append $HOME/.mozilla/firefox/${FF}/prefs.js >> /dev/null
+echo 'user_pref("spellchecker.dictionary", "en_CA");' | tee --append ${FFPATH}/prefs.js
 # sane Firefox NoScript settings
-echo 'user_pref("noscript.global", true);' | tee --append $HOME/.mozilla/firefox/${FF}/prefs.js >> /dev/null
-echo 'user_pref("noscript.ctxMenu", false);' | tee --append $HOME/.mozilla/firefox/${FF}/prefs.js >> /dev/null
+echo 'user_pref("noscript.global", true);' | tee --append ${FFPATH}/prefs.js
+echo 'user_pref("noscript.ctxMenu", false);' | tee --append ${FFPATH}/prefs.js
 # set userContent.css workaround for moz#70315
-mkdir -p $HOME/.mozilla/firefox/${FF}/chrome
+mkdir -p ${FFPATH}/chrome
 echo '/*
  * * Use this css file to eliminate problems in Firefox
  * * when using dark themes that create dark on dark
@@ -163,7 +164,7 @@ body {
 	display: block;
 	margin: 8px;
 	-moz-appearance: none !important;
-}' > $HOME/.mozilla/${FF}/chrome/userContent.css
+}' | tee ${FFPATH}/chrome/userContent.css
 
 ### set locale
 ## localectl is system-wide? dconf is local
@@ -178,35 +179,6 @@ dconf write /org/gnome/desktop/datetime/automatic-timezone true
 
 ### games - quite large
 sudo dnf install -y 0ad
-## steam - pulls in all the 32bit libs which is super gross
-# sudo dnf install -y http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-21.noarch.rpm
-# sudo dnf install -y steam
-
-### Netflix Google Chrome app - workaround until moz#1089858
-# add Google Chrome repo
-echo '[google-chrome]
-name=google-chrome
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-enabled=1
-gpgcheck=0' | sudo tee /etc/yum.repos.d/google-chrome.repo >> /dev/null
-# install Chrome
-sudo dnf install -y google-chrome
-# disable regular Chrome desktop file
-echo "NoDisplay=true" | sudo tee --append /usr/share/applications/google-chrome.desktop
-# add Netflix desktop file
-sudo mkdir -p /usr/local/share/applications
-echo "[Desktop Entry]
-Version=1.0
-Name=Netflix
-GenericName=Netflix
-Comment=Watch Netflix
-Exec=/usr/bin/google-chrome-stable --app=https://netflix.com %U
-Terminal=false
-Icon=/usr/local/share/icons/netflix.png
-Type=Application" | sudo tee /usr/local/share/applications/netflix.desktop
-# add icon
-sudo mkdir -p /usr/local/share/icons
-sudo curl -o /usr/local/share/icons/netflix.png https://gist.github.com/dgoerger/4c0e5df0ef6b09e3c0f5/raw/5900ccec25e4c7d3bc64478292241e90047fb24e/netflix.png
 
 ### now reboot to save and load changes
 reboot
