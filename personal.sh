973d917c86f933d5eb7715c61c61f68193835b83
Addison Hu <huisaddison@users.noreply.github.com>
2015-12-11 16:21:48 -0500

    more latex stuff

--- personal.sh
+++ personal.sh
@@ -49,7 +49,8 @@ sudo dnf install -y gstreamer1-libav gstreamer1-plugins-ugly gstreamer1-plugins-
                     gstreamer1-plugins-bad-free
 
 ### LaTeX
-sudo dnf install -y gummi texlive-scheme-basic texlive-collection-mathextra texlive-framed texlive-titling texlive-units
+sudo dnf install -y gummi texlive-scheme-basic texlive-collection-mathextra texlive-bbm texlive-bbm-macros \
+		    texlive-doublestroke texlive-framed texlive-titling texlive-units
 
 ### general apps
 # TODO: duplicity instead of deja-dup ?
