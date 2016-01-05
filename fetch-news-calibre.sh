20360eb50561321174359c05a4d863e39512c06a
Addison Hu <huisaddison@users.noreply.github.com>
2016-01-05 14:12:14 -0800

    wrapped functionality into a function, multiple calls for diff periodicals

--- fetch-news-calibre.sh
+++ fetch-news-calibre.sh
@@ -10,60 +10,57 @@ source ./credentials
 # FROM="username@gmail.com"
 ##
 
-####################### Begin Customization Section #############################
-#
-# Name of the recipe to fetch. You can run:
-#     ebook-convert --list-recipes
-# to look for the correct name. Do not forget the .recipe suffix
-RECIPE="New York Times.recipe"
+# Directory to store periodicals
 OUTDIR="$HOME/news"
-
-# Select your output profile. See http://manual.calibre-ebook.com/cli/ebook-convert-14.html
-# for a list. Common choices: kindle, kindle_dx, kindle_fire, kobo, ipad, sony
+# Common choices: kindle, kindle_dx, kindle_fire, kobo, ipad, sony
 OUTPROFILE="kindle"
-
-# A text file with an email per line. This script will send an email to each one.
-# You can set it to "" to not send any email
+# A text file with an email per line.
 EMAILSFILE="$HOME/news/emails.txt"
-
-# Your SMTP credentials
+# Miscellaneous SMTP credentials
 SMTP="smtp.gmail.com"
 PORT="587"
 
-# A prefix for the emails' subject. The date will be appended to it.
-SUBJECTPREFIX="News: The New York Times"
-# A prefix for the emails' content. The date will be appended to it.
 CONTENTPREFIX="Attached is the your periodical downloaded by calibre"
-# A prefix for generate file. The date will be appended to it.
-OUTPUTPREFIX="nyt_"
-#
-######################## End Customization Section #############################
 
 DATEFILE=`date "+%Y_%m_%d"`
 DATESTR=`date "+%Y/%m/%d"`
-OUTFILE="${OUTDIR}/${OUTPUTPREFIX}${DATEFILE}.mobi"
 
-echo "Fetching $RECIPE into $OUTFILE"
-ebook-convert "$RECIPE" "$OUTFILE" --output-profile "$OUTPROFILE" 
+function fetch_and_send {
+	RECIPE=$1
+	SUBJECTPREFIX=$2
+	OUTPUTPREFIX=$3	
+	OUTFILE="${OUTDIR}/${OUTPUTPREFIX}${DATEFILE}.mobi"
 
-# Change the author of the ebook from "calibre" to the current date. 
-# I do this because sending periodicals to a Kindle Touch is removing
-# the periodical format and there is no way to differentiate between
-# two editions in the home screen. This way, the date is shown next 
-# to the title.
-# See http://www.amazon.com/forum/kindle/ref=cm_cd_t_rvt_np?_encoding=UTF8&cdForum=Fx1D7SY3BVSESG&cdPage=1&cdThread=Tx1AP36U78ZHQ1I
-# and, please, email amazon (kindle-feedback@amazon.com) asking to add 
-# a way to keep the peridiocal format when sending through @free.kindle.com 
-# addresses
-echo "Setting date $DATESTR as author in $OUTFILE"
-ebook-meta -a "$DATESTR" "$OUTFILE"
+	echo "Fetching $RECIPE into $OUTFILE"
+	ebook-convert "$RECIPE" "$OUTFILE" --output-profile "$OUTPROFILE" 
 
-# email the files
-if [ -n "$EMAILSFILE" -a -f "$EMAILSFILE" ]; then
-    for TO in `cat $EMAILSFILE`; do
-	echo "Sending $OUTFILE to $TO"
-	calibre-smtp --attachment "$OUTFILE" --relay "$SMTP" --port "$PORT" --username "$USERNAME" --password "$PASSWORD"  --encryption-method TLS --subject "$SUBJECTPREFIX ($DATESTR)" "$FROM" "$TO"  "$CONTENTPREFIX ($DATESTR)"
-    done
+	echo "Setting date $DATESTR as author in $OUTFILE"
+	ebook-meta -a "$DATESTR" "$OUTFILE"
+
+	# email the files
+	if [ -n "$EMAILSFILE" -a -f "$EMAILSFILE" ]; then
+	    for TO in `cat $EMAILSFILE`; do
+		echo "Sending $OUTFILE to $TO"
+		calibre-smtp --attachment "$OUTFILE" --relay "$SMTP" --port "$PORT" --username "$USERNAME" --password "$PASSWORD"  --encryption-method TLS --subject "$SUBJECTPREFIX ($DATESTR)" "$FROM" "$TO"  "$CONTENTPREFIX ($DATESTR)"
+	    done
+	fi
+}
+
+fetch_and_send "New York Times.recipe" "News: The New York Times" "nyt_"
+notify-send "New York Times daily pull complete!"
+
+fetch_and_send "Los Angeles Times.recipe" "News: The Los Angeles Times" "lat_"
+notify-send "Los Angeles Times daily pull complete!"
+
+fetch_and_send "The Washington Post.recipe" "News: The Washington Post" "wp_"
+notify-send "Washington Post daily pull complete!"
+
+if [ $(date +%u) == 5 ]; then
+	fetch_and_send "The Economist.recipe" "News: The Economist" "economist_"
+	notify-send "The Economist weekly pull complete!"
 fi
 
-notify-send "New York Times daily pull complete!"
+if [ $(date +%d) == 01 ]; then
+	fetch_and_send "The Atlantic.recipe" "News: The Atlantic" "atlantic_"
+	notify-send "The Atlantic monthly pull complete!"
+fi
