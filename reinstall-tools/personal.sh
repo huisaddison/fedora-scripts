3ce6d7c18f11697552506e4eb0bef40d020ba969
Addison Hu <huisaddison@users.noreply.github.com>
2020-04-23 15:47:29 -0400

    update

--- reinstall-tools/personal.sh
+++ reinstall-tools/personal.sh
@@ -6,8 +6,9 @@ sudo dnf erase -y abrt* bijiben cheese devassistant evolution gnome-boxes gnome-
 				libvirt* orca qemu* rhythmbox
 
 ### enable moar repos
-sudo dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-31.noarch.rpm
-sudo dnf install -y http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-31.noarch.rpm
+sudo dnf install \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ### make sure everything's up-to-date
 sudo dnf upgrade -y
